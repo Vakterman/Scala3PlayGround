a262406f35faf42130f408b2727c7e67650474fe
Viktor Seroglazov <viktorseroglazov@gmail.com>
2021-10-13 00:18:34 +0400

    Updated test code

--- src/test/scala/FTests.scala
+++ src/test/scala/FTests.scala
@@ -1,9 +1,8 @@
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest._
-import seroglazov.org.FTest._
 
 class FTests extends AnyFunSuite {
     test("FTest contains item") {
-      val ftest: FSet[Int] = FSet(3)
+
     }
 }
